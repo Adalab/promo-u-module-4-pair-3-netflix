bcce000f46330eceb48aff681b2900aa56f194ec
Yasmin <yasmin27101991@gmail.com>
2023-11-08 10:28:27 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
-
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 
 const server = express();
@@ -10,6 +10,26 @@ server.use(cors());
 server.set('view engine', 'ejs');
 server.use(express.static('public'));
 server.use(express.json({ limit: '25mb' }));
+
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '3993yasmin',
+    database: 'Netflix',
+  });
+  connection.connect ();
+  return connection;
+}
+
+const generateToken = (payload) => {
+  const token = jwt.sign(payload, "secreto", { expiresIn: "12h" });
+  return token;
+};
+const serverPort = 4000;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
 
 const movies = [
   {
@@ -26,15 +46,7 @@ const movies = [
   }
 ];
 
-async function getConnection() {
-  const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'naiara-2019',
-    database: 'Netflix',
-  });
-  return connection;
-}
+
 
 server.get('/movies', async (req, res) => {
   const conn = await getConnection();
@@ -88,11 +100,54 @@ server.get('/movie/:movieId', async (req, res) => {
       message: 'Internal Server Error',
     });
   }
+
+//Proceso de login
+//usuario y la contraseña
+server.post('/sign-up', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Verifica si el email y la contraseña fueron proporcionados
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: 'Debe proporcionar un email y una contraseña',
+    });
+  }
+
+  try {
+    const conn = await getConnection();
+
+    // Encripta la contraseña
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Crea la nueva entrada en la base de datos
+    const [result] = await conn.query('INSERT INTO users (email, hashed_password) VALUES (?, ?)', [email, hashedPassword]);
+    conn.end();
+
+    const newUserId = result.insertId;
+
+    // Genera el token JWT
+    const token = generateToken({ userId: newUserId });
+
+    res.json({
+      success: true,
+      userId: newUserId,
+      token,
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
 });
 
-const serverPort = 4000;
-server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
-});
+//servidor de estáticos
+//servidor de estáticos
+const pathServerStatic = "./public_html";
+server.use(express.static(pathServerStatic));
 
+
+});
 
